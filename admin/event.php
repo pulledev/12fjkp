1904107e84118336aebb3e12e162000a5cac5c8c
Pulle <paul.treier@web.de>
2022-11-07 19:43:49 +0100

    Add the new Admin Panel Sites (event, files)

--- admin/event.php
+++ admin/event.php
@@ -13,3 +13,54 @@ if(!$id)
 
 }
 ?>
+<body>
+<?php
+Navbar::printNavbar();
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="text-center" style="padding-top: 40px;padding-bottom: 64px;">Events</h1>
+        </div>
+    </div>
+</div>
+
+
+<div class="table-responsive">
+    <table class="table">
+        <thead>
+        <tr>
+            <th>Id</th>
+            <th>Titel</th>
+            <th>Inhalt</th>
+            <th>Datum</th>
+            <th>Ersteller</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        $admins = AdminPanelServices::getInstance()->getMariadb()->listAdminUser();
+        if (empty($admins)) {
+            echo "<h4 class='text-center'>Fehlercode 2</h4>";
+        } else {
+            $cnt = 0;
+            foreach ($admins as $admin) {
+                $rank = AdminPanelServices::getInstance()->getTools()->decodeAdminRank($admin->getRank())
+                ?>
+                <tr>
+                    <td><?php echo $admin->getId() ?></td>
+                    <td><?php echo $admin->getUsername() ?></td>
+                    <td> <?php echo $rank ?></td>
+                </tr>
+                <?php
+            }
+        }
+
+        ?>
+
+        </tbody>
+    </table>
+</div>
+
+
+</body>
